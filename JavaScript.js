4a4eb3caa905a2ffda56275cc8c5728e01f3fd81
ahujarahul9711 <73956558+ahujarahul9711@users.noreply.github.com>
2020-11-08 23:00:35 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,16 @@
+var showSideBar = function ()
+{
+	document.getElementById('sidebarid').style.display="inline";
+	document.getElementById('sidebarid').style.width="15rem";
+}
+var closeSideBar = function ()
+{
+	document.getElementById('sidebarid').style.display="none";
+	document.getElementById('sidebarid').style.width="0";
+}
+var checkSideBar = function()
+{
+	document.getElementById('sidebarid').style.display="inline";
+	document.getElementById('sidebarid').style.width="15rem";
+}
+
